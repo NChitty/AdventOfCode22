2a6c4356386588763de889d8f14c9c7f4881b8a6
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-07-19 13:47:50 -0400

    update libraries

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,17 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 
-pub mod day3;
+// day 1
+pub mod calorie_counting;
+// day 2
+pub mod rock_paper_scissors;
+// day 3
+pub mod rucksack_reorg;
+// day 4
 pub mod camp_cleanup;
+// day 5
 pub mod supply_stacks;
+// day 6
 pub mod tuning_trouble;
 
 aoc_lib!{ year = 2022 }
