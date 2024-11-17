6f7471679a1bf6c3a3f97c74b2bb38f381607377
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-11-17 17:25:57 -0500

    Part 1 complete

--- src/day15.rs
+++ src/day15.rs
@@ -1,141 +1,120 @@
-use std::{cmp::min, collections::HashMap, isize};
+use std::{isize, ops::Range, str::FromStr};
 
+use itertools::Itertools;
 use regex::Regex;
 
 const LOCATION_REGEX: &str = "Sensor at x=(?<sensor_x>[-\\d]*?), y=(?<sensor_y>[-\\d]*?): closest beacon is at x=(?<beacon_x>[-\\d]*?), y=(?<beacon_y>[-\\d]*?)$";
 
-fn get_disallowed_locations(
-    sensor_beacon_map: &HashMap<(isize, isize), (isize, isize)>,
-    min_pos: (isize, isize),
-    max_pos: (isize, isize),
-) -> Vec<Vec<bool>> {
-    let mut possible_locations =
-        vec![vec![true; max_pos.0.abs_diff(min_pos.0) + 1]; max_pos.1.abs_diff(min_pos.1) + 1];
-    sensor_beacon_map.keys().for_each(|key| {
-        let beacon_pos: (isize, isize) = *sensor_beacon_map.get(key).expect("No value for key");
-        let max_delta: isize = (beacon_pos.0.abs_diff(key.0) + beacon_pos.1.abs_diff(key.1))
-            .try_into()
-            .expect("Could not convert to isize.");
-        let min_beacon_x = min(key.0 - max_delta, min_pos.0);
-        let max_beacon_x = min(key.0 + max_delta, max_pos.0);
-        let min_beacon_y = min(key.1 - max_delta, min_pos.1);
-        let max_beacon_y = min(key.1 + max_delta, max_pos.1);
-        (min_beacon_x..=max_beacon_x).for_each(|x| {
-            (min_beacon_y..=max_beacon_y)
-                .filter(|y| {
-                    let x_delta: isize = x
-                        .abs_diff(key.0)
-                        .try_into()
-                        .expect("Could not convert to isize");
-                    let y_delta: isize = y
-                        .abs_diff(key.1)
-                        .try_into()
-                        .expect("Could not convert to isize");
-                    x_delta + y_delta <= max_delta
-                })
-                .for_each(|y| {
-                    let vec_x = x.abs_diff(min_pos.0);
-                    let vec_y = y.abs_diff(min_pos.1);
-                    possible_locations[vec_y][vec_x] = false;
-                });
-        });
-    });
-    sensor_beacon_map.values().for_each(|pos| {
-        let vec_x = pos.0.abs_diff(min_pos.0);
-        let vec_y = pos.1.abs_diff(min_pos.1);
-        possible_locations[vec_y][vec_x] = true;
-    });
-    possible_locations
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Point {
+    x: isize,
+    y: isize,
 }
 
-#[aoc_generator(day15, part1)]
-fn get_sensor_beacon_map(
-    input: &str,
-) -> (
-    HashMap<(isize, isize), (isize, isize)>,
-    (isize, isize),
-    (isize, isize),
-) {
-    let mut min_pos = (isize::MAX, isize::MAX);
-    let mut max_pos = (isize::MIN, isize::MIN);
-    let matcher = Regex::new(LOCATION_REGEX).expect("Could not build regex.");
-    let mut map = HashMap::new();
-    input.lines().for_each(|line| {
-        if let Some(cap) = matcher.captures(line) {
-            let sensor_pos: (isize, isize) = (
-                cap.name("sensor_x")
-                    .expect("No match for sensor x position")
-                    .as_str()
-                    .parse()
-                    .expect("Could not parse sensor x as integer"),
-                cap.name("sensor_y")
-                    .expect("No match for sensor y position")
-                    .as_str()
-                    .parse()
-                    .expect("Could not parse sensor y as integer"),
-            );
-            if sensor_pos.0 < min_pos.0 {
-                min_pos.0 = sensor_pos.0;
+impl Point {
+    fn manhattan_distance(&self, other: &Point) -> usize {
+        self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Pair {
+    sensor: Point,
+    beacon: Point,
+}
+
+impl FromStr for Pair {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let matcher = Regex::new(LOCATION_REGEX).map_err(|_| "Could not build regex.")?;
+        let captures = matcher.captures(s).ok_or("No matches found.")?;
+        let sensor: Point = Point {
+            x: captures
+                .name("sensor_x")
+                .ok_or("No match for sensor x position")?
+                .as_str()
+                .parse()
+                .map_err(|_| "Could not parse sensor x as integer")?,
+            y: captures
+                .name("sensor_y")
+                .ok_or("No match for sensor y position")?
+                .as_str()
+                .parse()
+                .map_err(|_| "Could not parse sensor y as integer")?,
+        };
+        let beacon: Point = Point {
+            x: captures
+                .name("beacon_x")
+                .ok_or("No match for beacon x position")?
+                .as_str()
+                .parse()
+                .map_err(|_| "Could not parse beacon x as integer")?,
+            y: captures
+                .name("beacon_y")
+                .ok_or("No match for beacon y position")?
+                .as_str()
+                .parse()
+                .map_err(|_| "Could not parse beacon y as integer")?,
+        };
+        Ok(Self { sensor, beacon })
+    }
+}
+
+impl Pair {
+    fn covered_distance(&self) -> usize {
+        self.sensor.manhattan_distance(&self.beacon)
+    }
+
+    fn is_covered(&self, location: &Point) -> bool {
+        self.sensor.manhattan_distance(location) <= self.covered_distance()
+    }
+
+    fn get_covered_ranges_in_row(&self, row: isize) -> Option<Range<isize>> {
+        let x_offset = self.covered_distance() as isize - (self.sensor.y - row).abs();
+        Some((self.sensor.x - x_offset)..(self.sensor.x + x_offset + 1)).filter(|r| !r.is_empty())
+    }
+}
+
+#[aoc_generator(day15)]
+fn parse_input(input: &str) -> Vec<Pair> {
+    input
+        .lines()
+        .map(|line| line.parse().expect("Could not parse line"))
+        .collect()
+}
+
+fn solve(pairs: &[Pair], row: isize) -> usize {
+    let covered_in_row: usize = pairs
+        .iter()
+        .flat_map(|pair| pair.get_covered_ranges_in_row(row))
+        .sorted_unstable_by_key(|range| range.start)
+        .coalesce(|a, b| {
+            if a.end >= b.start {
+                Ok(a.start..b.end.max(a.end))
+            } else {
+                Err((a, b))
             }
-            if sensor_pos.1 < min_pos.1 {
-                min_pos.1 = sensor_pos.1;
-            }
-            if sensor_pos.0 > max_pos.0 {
-                max_pos.0 = sensor_pos.0;
-            }
-            if sensor_pos.1 > max_pos.1 {
-                max_pos.1 = sensor_pos.1;
-            }
-            let beacon_pos: (isize, isize) = (
-                cap.name("beacon_x")
-                    .expect("No match for beacon x position")
-                    .as_str()
-                    .parse()
-                    .expect("Could not parse beacon x as integer"),
-                cap.name("beacon_y")
-                    .expect("No match for beacon y position")
-                    .as_str()
-                    .parse()
-                    .expect("Could not parse beacon y as integer"),
-            );
-            if beacon_pos.0 < min_pos.0 {
-                min_pos.0 = beacon_pos.0;
-            }
-            if beacon_pos.1 < min_pos.1 {
-                min_pos.1 = beacon_pos.1;
-            }
-            if beacon_pos.0 > max_pos.0 {
-                max_pos.0 = beacon_pos.0;
-            }
-            if beacon_pos.1 > max_pos.1 {
-                max_pos.1 = beacon_pos.1;
-            }
-            map.insert(sensor_pos, beacon_pos);
-        }
-    });
-    (map, min_pos, max_pos)
+        })
+        .map(|xs| xs.len())
+        .sum();
+    let blocked_xs = pairs
+        .into_iter()
+        .flat_map(|pair| [pair.sensor, pair.beacon])
+        .filter(|p| p.y == row)
+        .unique()
+        .count();
+    covered_in_row - blocked_xs
 }
 
 #[aoc(day15, part1)]
-fn count_disallowed_locations_in_row(
-    input: &(
-        HashMap<(isize, isize), (isize, isize)>,
-        (isize, isize),
-        (isize, isize),
-    ),
-) -> usize {
-    let possible_locations = get_disallowed_locations(&input.0, input.1, input.2);
-    let vec_y = 2_000_000isize.abs_diff(input.1 .1);
-    let disallowed = possible_locations[vec_y]
-        .iter()
-        .filter(|&&is_allowed| !is_allowed)
-        .count();
-    disallowed
+fn solve_part1(input: &[Pair]) -> usize {
+    solve(input, 2_000_000)
 }
 
 #[cfg(test)]
 mod test {
-    use super::{get_disallowed_locations, get_sensor_beacon_map};
+    use super::{parse_input, solve};
 
     const SAMPLE_INPUT: &str = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
@@ -153,15 +132,10 @@ Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3";
 
     #[test]
-    fn sample_count_part1() {
-        let obs = get_sensor_beacon_map(SAMPLE_INPUT);
+    fn sample_part1() {
+        let input = parse_input(SAMPLE_INPUT);
         let expected = 26;
-        let possible_locations = get_disallowed_locations(&obs.0, obs.1, obs.2);
-        let vec_y = 10isize.abs_diff(obs.1 .1);
-        let actual = possible_locations[vec_y]
-            .iter()
-            .filter(|&&is_allowed| !is_allowed)
-            .count();
+        let actual = solve(&input, 10);
         assert_eq!(expected, actual);
     }
 }
