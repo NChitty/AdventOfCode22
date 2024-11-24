7b2a727b6416da5a8096a8153fa005133f75c644
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-11-24 18:56:59 -0500

    day16 part1 faster algorithm

---
+++ src/day16.rs
@@ -0,0 +1,199 @@
+use std::{collections::HashMap, hash::Hash, str::FromStr, usize};
+
+#[derive(Clone, Debug, PartialEq, PartialOrd)]
+struct Valve {
+    name: String,
+    rate: usize,
+    adjacent: Vec<String>,
+}
+
+#[derive(Clone, Debug)]
+struct State {
+    current_valve: String,
+    minutes_remaining: usize,
+    current_rate: usize,
+    released: usize,
+    opened: Vec<String>,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            current_valve: "AA".to_string(),
+            minutes_remaining: 30,
+            current_rate: 0,
+            released: 0,
+            opened: Vec::new(),
+        }
+    }
+}
+
+impl FromStr for Valve {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(";").collect();
+        if parts.len() != 2 {
+            return Err("No semicolon to split".to_string());
+        }
+
+        // Parse valve name and flow rate
+        let first_part_parts: Vec<&str> = parts[0].split_whitespace().collect();
+        if first_part_parts.len() < 5 {
+            return Err("There are not enough tokens.".to_string());
+        }
+        let name = first_part_parts[1].to_string();
+        let rate_str = first_part_parts[4];
+        let rate = rate_str
+            .split('=')
+            .nth(1)
+            .ok_or("Could not get rate")?
+            .parse::<usize>()
+            .map_err(|_| "Could not parse rate to usize")?;
+
+        // Parse adjacent valves
+        let second_part = parts[1];
+        let adjacent_str = if second_part.contains("valves") {
+            second_part.split("valves ").nth(1)
+        } else {
+            second_part.split("valve ").nth(1)
+        }
+        .ok_or("No second part for adjacent")?;
+
+        let adjacent: Vec<String> = adjacent_str
+            .split(", ")
+            .map(|s| s.trim().to_string())
+            .collect();
+
+        Ok(Valve {
+            name,
+            rate,
+            adjacent,
+        })
+    }
+}
+
+fn initial_adjacency_matrix(map: &HashMap<String, Valve>) -> HashMap<(String, String), usize> {
+    let mut adjacency_matrix = HashMap::new();
+    map.keys().for_each(|key| {
+        map[key].adjacent.iter().for_each(|adjacent_valve| {
+            adjacency_matrix.insert((key.clone(), adjacent_valve.clone()), 1);
+        })
+    });
+    adjacency_matrix
+}
+
+fn fill_matrix(
+    map: &HashMap<String, Valve>,
+    adjacency_matrix: &mut HashMap<(String, String), usize>,
+) {
+    for k in map.keys() {
+        for i in map.keys() {
+            for j in map.keys() {
+                let distance = adjacency_matrix.entry((i.to_string(), j.to_string()));
+                let intermediate_first = adjacency_matrix.entry((i.to_string(), k.to_string()));
+                let intermediate_second = adjacency_matrix.entry((k.to_string(), j.to_string()));
+            }
+        }
+    }
+}
+
+fn brute_search(state: &State, map: &HashMap<String, Valve>) -> usize {
+    let mut stack = Vec::new();
+    let mut max_released = 0;
+
+    stack.push(state.clone());
+    while let Some(mut current_state) = stack.pop() {
+        current_state.minutes_remaining -= 1;
+        current_state.released += current_state.current_rate;
+
+        if current_state.minutes_remaining == 0 {
+            max_released = max_released.max(current_state.released);
+            continue;
+        }
+
+        if !current_state.opened.contains(&current_state.current_valve)
+            && map
+                .get(&current_state.current_valve)
+                .expect("Could not find current_valve")
+                .rate
+                != 0
+        {
+            let mut opened_state = current_state.clone();
+            opened_state
+                .opened
+                .push(current_state.current_valve.clone());
+            opened_state.current_rate += map
+                .get(&current_state.current_valve)
+                .expect("Could not find current_valve")
+                .rate;
+            stack.push(opened_state);
+        }
+
+        if let Some(adjacent) = map.get(&current_state.current_valve) {
+            for valve in &adjacent.adjacent {
+                let mut next_state = current_state.clone();
+                next_state.current_valve = valve.clone();
+                stack.push(next_state);
+            }
+        }
+    }
+    max_released
+}
+
+#[aoc_generator(day16)]
+fn parse_valves(input: &str) -> HashMap<String, Valve> {
+    input
+        .lines()
+        .filter_map(|line| Valve::from_str(line).ok())
+        .map(move |valve| (valve.name.clone(), valve))
+        .collect()
+}
+
+#[aoc(day16, part1, BruteForce)]
+fn part1_brute_force(input: &HashMap<String, Valve>) -> usize {
+    let state = State::default();
+    brute_search(&state, &input)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::day16::part1_brute_force;
+
+    use super::{parse_valves, Valve};
+
+    const SAMPLE_INPUT: &str = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II";
+
+    #[test]
+    fn aa_from_str() {
+        let expected = Valve {
+            name: "AA".to_string(),
+            rate: 0,
+            adjacent: vec!["DD".to_string(), "II".to_string(), "BB".to_string()],
+        };
+        let actual = SAMPLE_INPUT
+            .lines()
+            .next()
+            .expect("No lines")
+            .parse()
+            .expect("Could not parse line.");
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn sample_brute() {
+        let input = parse_valves(SAMPLE_INPUT);
+        let expected = 1651;
+        let actual = part1_brute_force(&input);
+        assert_eq!(expected, actual);
+    }
+}
